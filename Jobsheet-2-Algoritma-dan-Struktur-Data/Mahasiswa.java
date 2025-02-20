a77d7c36c7f7888e0c173844b51d0ef7818fe13d
MariaSavira <mariasavira2006@gmail.com>
2025-02-20 07:37:13 +0700

    Soal Praktikum 1 nomor 4

--- Jobsheet-2-Algoritma-dan-Struktur-Data/Mahasiswa.java
+++ Jobsheet-2-Algoritma-dan-Struktur-Data/Mahasiswa.java
@@ -1,34 +1,37 @@
-public class Mahasiswa {
+public class Mahasiswa{
     String nama;
     String nim;
     String kelas;
     double ipk;
 
-    void tampilkanInformasi() {
+    void tampilkanInformasi(){
         System.out.println("Nama : " + nama);
         System.out.println("NIM : " + nim);
         System.out.println("IPK : " + ipk);
         System.out.println("Kelas : " + kelas);
     }
 
-    void ubahKelas(String kelasBaru) {
+    void ubahKelas(String kelasBaru){
         kelas = kelasBaru;
     }
 
-    void updateIPK(double ipkBaru) {
+    void updateIPK(double ipkBaru){
         ipk = ipkBaru;
     }
 
-    String nilaiKinerja() {
-        if (ipk >= 3.5) {
-            return "Kinerja sangat baik";
-        } else if (ipk >= 3.0) {
-            return "Kinerja baik";
-        } else if (ipk >= 2.5) {
-            return "Kinerja cukup";
+    String nilaiKinerja(){
+        if (ipk > 4.0 || ipk < 0){
+            return "IPK tidak valid. Harus antara 0.0 dan 4.0";
         } else {
-            return "Kinerja kurang";
+            if (ipk >= 3.5){
+                return "Kinerja sangat baik";
+            } else if (ipk >= 3.0){
+                return "Kinerja baik";
+            } else if (ipk >= 2.5){
+                return "Kinerja cukup";
+            } else {
+                return "Kinerja kurang";
+            }
         }
-
     }
 }
