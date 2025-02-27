eb7888888026de42d83333b61c9d7ae45a8d37a8
MariaSavira <mariasavira2006@gmail.com>
2025-02-27 11:56:38 +0700

    Percobaan 3 Nomor 2-4

--- Jobsheet-3-Algoritma-dan-Struktur-Data/Matakuliah07.java
+++ Jobsheet-3-Algoritma-dan-Struktur-Data/Matakuliah07.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Matakuliah07{
+    static Scanner scan = new Scanner(System.in);
     public String kode;
     public String nama;
     public int sks;
@@ -9,5 +12,39 @@ public class Matakuliah07{
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+    }
+
+    static void tambahData(Matakuliah07[] matkul){
+        String kode, nama, dummy;
+        int sks, jumlahJam;
+
+        for (int i = 0; i < matkul.length; i++){
+            System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
+            System.out.print("Kode        : ");
+            kode = scan.nextLine();
+            System.out.print("Nama        : ");
+            nama = scan.nextLine();
+            System.out.print("SKS         : ");
+            dummy = scan.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam  : ");
+            dummy = scan.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("=============================");
+
+            matkul[i] = new Matakuliah07(kode, nama, sks, jumlahJam);
+        }
+        return;
+    }
+
+    static void cetakInfo(Matakuliah07[] matkul){
+        for (int i = 0; i < matkul.length; i++) {
+            System.out.println("Data Matakuliah ke-" + (i + 1));
+            System.out.println("Kode       : " + matkul[i].kode);
+            System.out.println("Nama       : " + matkul[i].nama);
+            System.out.println("SKS        : " + matkul[i].sks);
+            System.out.println("Jumlah Jam : " + matkul[i].jumlahJam);
+            System.out.println("=============================");
+        }
     }
 }
--- Jobsheet-3-Algoritma-dan-Struktur-Data/MatakuliahDemo07.java
+++ Jobsheet-3-Algoritma-dan-Struktur-Data/MatakuliahDemo07.java
@@ -3,34 +3,11 @@ import java.util.Scanner;
 public class MatakuliahDemo07{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Matakuliah07[] arrayOfMatakuliah = new Matakuliah07[3];
-        String kode, nama, dummy;
-        int sks, jumlahJam;
+        System.out.print("Masukkan jumlah matakuliah : ");
+        int index = scan.nextInt();
+        Matakuliah07[] arrayOfMatakuliah = new Matakuliah07[index];
 
-        for (int i = 0; i < arrayOfMatakuliah.length; i++){
-            System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
-            System.out.print("Kode        : ");
-            kode = scan.nextLine();
-            System.out.print("Nama        : ");
-            nama = scan.nextLine();
-            System.out.print("SKS         : ");
-            dummy = scan.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("Jumlah Jam  : ");
-            dummy = scan.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
-            System.out.println("=============================");
-
-            arrayOfMatakuliah[i] = new Matakuliah07(kode, nama, sks, jumlahJam);
-        }
-
-        for (int i = 0; i < arrayOfMatakuliah.length; i++) {
-            System.out.println("Data Matakuliah ke-" + (i + 1));
-            System.out.println("Kode       : " + arrayOfMatakuliah[i].kode);
-            System.out.println("Nama       : " + arrayOfMatakuliah[i].nama);
-            System.out.println("SKS        : " + arrayOfMatakuliah[i].sks);
-            System.out.println("Jumlah Jam : " + arrayOfMatakuliah[i].jumlahJam);
-            System.out.println("=============================");
-        }
+        Matakuliah07.tambahData(arrayOfMatakuliah);
+        Matakuliah07.cetakInfo(arrayOfMatakuliah);
     }
 }
