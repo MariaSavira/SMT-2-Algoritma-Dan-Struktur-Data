7f0a5d5a9627101954a4b125987568a8c549fa40
MariaSavira <mariasavira2006@gmail.com>
2025-05-15 13:55:53 +0700

    Commit ulang Jobsheet 11 Percobaan 1 Modifikasi

--- Jobsheet-10-Queue/Tugas/AntrianKRS.java
+++ Jobsheet-10-Queue/Tugas/AntrianKRS.java
@@ -1,33 +1,128 @@
-package Pertemuan12;
-import java.util.Scanner;
+package Tugas;
 
-public class SLLMain7 {
-    public static void main(String[] args) {
-        SingleLinkedList7 sll = new SingleLinkedList7();
-        Scanner scan = new Scanner(System.in);
-        int pilih = 0;
+public class AntrianKRS {
+    Mahasiswa[] data;
+    int front;
+    int rear;
+    int size;
+    int max;
+    int jmlDilayani = 0;
 
-        Mahasiswa7 mhs1 = new Mahasiswa7("101", "Maria", "SIB-1A", 3.75);
-        Mahasiswa7 mhs2 = new Mahasiswa7("102", "Savira", "SIB-1B", 3.65);
-        Mahasiswa7 mhs3 = new Mahasiswa7("103", "Sasa", "SIB-1C", 3.90);
-        Mahasiswa7 mhs4 = new Mahasiswa7("104", "Marsha", "SIB-1D", 3.55);
+    public AntrianKRS(int max){
+        this.max = max;
+        this.data = new Mahasiswa[max];
+        this.front = 0;
+        this.rear = -1;
+        this.size = 0;
+    }
 
-        do {
-            System.out.println("Pilih menu : ");
-            System.out.println("1. Input");
-            System.out.println("Pilih menu : ");
-            System.out.println("Pilih menu : ");
+    public boolean isEmpty(){
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
         }
-        while (pilih == 0);
-            System.out.println("Terima kasih!");
+    }
 
-        sll.print();
-        sll.addFirst(mhs4);
-        sll.print();
-        sll.addFirst(mhs1);
-        sll.print();
-        sll.insertAfter("Marsha", mhs3);
-        sll.insertAt(2, mhs2);
-        sll.print();
+    public boolean isFull(){
+        if (size == max) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void clear(){
+        if (!isEmpty()) {
+            front = rear = -1;
+            size = 0;
+            System.out.println("Antrian berhasil dikosongkan");
+        } else {
+            System.out.println("Antrian masih kosong");
+        }
+    }
+
+    public void tambahAntrian(Mahasiswa mhs){
+        if (isFull()) {
+            System.out.println("Antrian penuh, tidak dapat menambah mahasiswa.");
+            return;
+        }
+        rear = (rear + 1) % max;
+        data[rear] = mhs;
+        size++;
+        System.out.println(mhs.nama + " berhasil masuk antrian");
+    }
+
+    public Mahasiswa layaniMahasiswa(){
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+        }
+        Mahasiswa mhs = data[front];
+        front = (front + 1) % max;
+        size--;
+        return mhs;
+    }
+
+    public void prosesKRS2Mahasiswa() {
+        if (size < 2) {
+            System.out.println("Minimal 2 mahasiswa dibutuhkan untuk memproses KRS.");
+            return;
+        }
+    
+        System.out.println("Memproses KRS untuk 2 mahasiswa:");
+        for (int i = 0; i < 2; i++) {
+            Mahasiswa mhs = layaniMahasiswa();
+            System.out.print(" - ");
+            mhs.tampilkanData();
+            jmlDilayani++;
+        }
+    }
+
+    public void tampilkanSemua(){
+        if (isEmpty()) {
+            System.out.println("Queue masih kosong");
+            return;
+        }
+        System.out.println("Daftar Mahasiswa dalam Antrian:");
+        System.out.println("NIM - NAMA - PRODI - KELAS");
+        for (int i = 0; i < size; i++) {
+            int index = (front + i) % max;
+            System.out.print((i+1) + ". ");
+            data[index].tampilkanData();
+        }
+    }
+
+    public void lihat2Terdepan(){
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+        } else {
+            System.out.println("Mahasiswa terdepan: ");
+            System.out.println("NIM - NAMA - PRODI - KELAS");
+            for (int i = front; i < front+2; i++) {
+                data[i].tampilkanData();
+            }
+        }
+    }
+
+    public void lihatAkhir(){
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+        } else {
+            System.out.println("Mahasiswa terdepan: ");
+            System.out.println("NIM - NAMA - PRODI - KELAS");
+            data[rear].tampilkanData();
+        }
+    }
+
+    public int getJumlahAntrian(){
+        return size;
+    }
+
+    public int getJmlDilayani(){
+        return jmlDilayani;
+    }
+
+    public int getSisaLayanan(){
+        return 30 - jmlDilayani;
     }
 }
