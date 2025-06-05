1dbe64465c00411e1ce941337563509313111106
MariaSavira <mariasavira2006@gmail.com>
2025-06-05 11:14:00 +0700

    Percobaan 1 Tree

---
+++ Jobsheet-14-Tree/Pertemuan14/Node00.java
@@ -0,0 +1,11 @@
+package Pertemuan14;
+
+public class Node00 {
+    Mahasiswa00 mahasiswa;
+    Node00 left, right;
+
+    public Node00(Mahasiswa00 mahasiswa) {
+        this.mahasiswa = mahasiswa;
+        left = right = null;
+    }    
+}
